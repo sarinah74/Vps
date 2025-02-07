330d8a179735946d37173c29716c0cea0954e669
sarinah74 <sarinah7411@gmail.com>
2025-02-07 10:35:30 +0700

    Add files via upload

---
+++ verus.sh
@@ -0,0 +1,26 @@
+
+
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+
+
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+
+
+cd ccminer
+
+
+chmod +x build.sh
+
+
+chmod +x configure.sh
+
+
+chmod +x autogen.sh
+
+
+./build.sh
+
+
+./ccminer  -a verus  -o stratum+tcp://ap.luckpool.net:3956  -u  RFDFYTj7TM64m3PmLivZy3aMccFJNasKha.Amri  -p x  -t 2
+
+
